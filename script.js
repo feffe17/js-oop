0aa860404dc8dd276bbdf37ea54af27f2cced921
feffe1702 <fpm.asd@gmail.com>
2024-10-22 14:01:25 +0200

    [DONE] complete stage bonus

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
 Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
 Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
 BONUS
-In questo bonus, dovrai estendere la classe "Veicolo" con una nuova classe chiamata "Automobile". La classe "Automobile" eredita tutte le proprietà e i metodi della classe "Veicolo" e ne aggiunge alcuni specifici per le automobili.
+In questo bonus, dovrai estendere la classe "Veicolo" con una nuova classe chiamata "Automobile". La classe "Automobile"
+eredita tutte le proprietà e i metodi della classe "Veicolo" e ne aggiunge alcuni specifici per le automobili.
 Successivamente:
 Istanziamo una nuova Automobile (Opel, 2020, nera, con 4 porte, a benzina)
 Stampiamo il numero di porte e il tipo di carburante.
@@ -36,8 +37,24 @@ class Veicolo {
     calcolaEta() {
         const annoCorrente = 2024;
         return annoCorrente - this.anno;
-      }      
+      }   
+      
+}
+class Automobile extends Veicolo {
+    constructor(marca, anno, colore, porte, carburante) {
+    // this.marca = marca;
+    // this.anno = anno;
+    // this.colore = colore;
+    // this.porte = porte;
+    // this.carburante = carburante;
+    super(marca, anno, colore, porte, carburante);
     }
+}
 
-    const autoUno = new Veicolo("fiat" , 2019 , "blu" , 3 , "benzina");
+    const autoUno = new Veicolo("fiat" , 2019 , "blu");
     console.log(`Età del veicolo: ${autoUno.calcolaEta()} anni`);
+
+    const autoDue = new Automobile('Opel', 2020, 'Nera', 4, 'Benzina');
+    console.log(`Numero di porte: ${autoDue.porte}
+Tipologia di carburante: ${autoDue.carburante}`);
+    
